43056464a802f43fd2f5d8ec4fe38bd92ab9925f
Giuseppe Di Natale <guss80@gmail.com>
2018-01-13 16:23:53 -0800

    Add project files.

---
+++ Win32Lib/Win32.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace Win32Lib
+{
+    public class Win32
+    {
+        public delegate IntPtr WndProc(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("coredll.dll")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetCapture();
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowDC(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        public static extern int ReleaseDC(IntPtr hWnd, IntPtr hDC);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("user32.dll")]
+        public static extern bool GetClientRect(IntPtr hWnd, out RECT lpRect);
+
+        [DllImport("gdi32.dll")]
+        public static extern int ExcludeClipRect(IntPtr hdc, int nLeftRect, int nTopRect,
+          int nRightRect, int nBottomRect);
+
+        [DllImport("comctl32.dll", CharSet = CharSet.Auto)]
+        public static extern int DllGetVersion(ref DLLVersionInfo version);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern System.UInt16 RegisterClassW([In] ref WNDCLASS lpWndClass);
+
+        [DllImport("User32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int x, int y, int cx, int cy, SetWindowPosFlags Flags);
+
+        [DllImport("User32.dll", SetLastError = true)]
+        public static extern bool ShowWindow(IntPtr hWnd, ShowWindowCommand nCmdShow);
+
+        [DllImport("User32.dll", SetLastError = true)]
+        public static extern IntPtr CreateWindowEx(WindowStylesEx dwExStyle, string lpClassName, string lpWindowName, int dwStyle, int x, int y, int nWidth, int nHeight,
+          IntPtr hWndParent, IntPtr hMenu, IntPtr hInstance, IntPtr lpParam);
+
+        [DllImport("User32.dll", SetLastError = true)]
+        public static extern int SendMessage(IntPtr hWnd, uint Msg, int wParam, IntPtr lParam);
+
+        [DllImport("User32.dll", SetLastError = true)]
+        public static extern int DestroyWindow(IntPtr hwnd);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr DefWindowProcW(IntPtr hWnd, int msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("User32.dll", SetLastError = true)]
+        public extern static int ValidateRect(System.IntPtr hWnd, System.IntPtr rect);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool InvalidateRect(IntPtr hWnd, IntPtr lpRect, bool bErase);
+
+        [DllImport("user32.dll")]
+        public static extern int SetWindowRgn(IntPtr hWnd, IntPtr hRgn, bool bRedraw);
+
+        [DllImport("user32.dll")]
+        public static extern int SetWindowRgn(IntPtr hWnd, ref RECT hRgn, bool bRedraw);
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData,
+          int dwExtraInfo);
+
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint dwData,
+           IntPtr dwExtraInfo);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern IntPtr PostMessage(IntPtr hWnd, uint Msg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        public static extern bool RedrawWindow(IntPtr hWnd, [In] ref RECT lprcUpdate, IntPtr hrgnUpdate, int flags);
+
+        [DllImport("user32.dll")]
+        public static extern bool RedrawWindow(IntPtr hWnd, IntPtr lprcUpdate, IntPtr hrgnUpdate, int flags);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        public static extern IntPtr GetMessageExtraInfo();
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern bool TrackMouseEvent(ref TRACKMOUSEEVENT lpEventTrack);
+
+        /// <summary>
+        /// SendsInput using Win32 API Function
+        /// </summary>
+        /// <param name="nInputs">The number input structures in the array.</param>
+        /// <param name="pInputs">The pointer to array of input structures.</param>
+        /// <param name="cbSize">Size of the structure in the array.</param>
+        /// <returns>The function returns the number of events that it successfully
+        /// inserted into the keyboard or mouse input stream. If the function returns zero,
+        /// the input was already blocked by another thread. To get extended error information,
+        /// call GetLastError.</returns>
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint SendInput(uint nInputs, ref INPUT pInputs, int cbSize);
+
+        public static int GetLowWord(int intValue)
+        {
+            return (intValue & 0x0000FFFF);
+        }
+
+        public static int GetHighWord(int value)
+        {
+            return (value >> 16) & 0xffff;
+        }
+
+    }
+}
---
+++ Win32Lib/Win32Structures.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace Win32Lib
+{
+    [StructLayout(LayoutKind.Sequential)]
+    public struct NCCALCSIZE_PARAMS
+    {
+        public RECT rgrc0, rgrc1, rgrc2;
+        public IntPtr lppos;
+    }
+
+    [StructLayout(LayoutKind.Explicit)]
+    public struct SIZE
+    {
+        [FieldOffset(0)]
+        public int X;
+        [FieldOffset(4)]
+        public int Y;
+
+        public SIZE(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public SIZE(Size s)
+        {
+            X = s.Width;
+            Y = s.Height;
+        }
+
+        public Size ToSize()
+        {
+            return new Size(X, Y);
+        }
+    }
+
+    [Serializable, StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
+
+        public RECT(int left_, int top_, int right_, int bottom_)
+        {
+            Left = left_;
+            Top = top_;
+            Right = right_;
+            Bottom = bottom_;
+        }
+
+        public int Height { get { return Bottom - Top + 1; } }
+        public int Width { get { return Right - Left + 1; } }
+        public Size Size { get { return new Size(Width, Height); } }
+
+        public Point Location { get { return new Point(Left, Top); } }
+
+        // Handy method for converting to a System.Drawing.Rectangle
+        public Rectangle ToRectangle()
+        { return Rectangle.FromLTRB(Left, Top, Right, Bottom); }
+
+        public static RECT FromRectangle(Rectangle rectangle)
+        {
+            return new RECT(rectangle.Left, rectangle.Top, rectangle.Right, rectangle.Bottom);
+        }
+
+        public void Inflate(int width, int height)
+        {
+            this.Left -= width;
+            this.Top -= height;
+            this.Right += width;
+            this.Bottom += height;
+        }
+
+        public override int GetHashCode()
+        {
+            return Left ^ ((Top << 13) | (Top >> 0x13))
+              ^ ((Width << 0x1a) | (Width >> 6))
+              ^ ((Height << 7) | (Height >> 0x19));
+        }
+
+        #region Operator overloads
+
+        public static implicit operator Rectangle(RECT rect)
+        {
+            return Rectangle.FromLTRB(rect.Left, rect.Top, rect.Right, rect.Bottom);
+        }
+
+        public static implicit operator RECT(Rectangle rect)
+        {
+            return new RECT(rect.Left, rect.Top, rect.Right, rect.Bottom);
+        }
+
+        #endregion
+    }
+
+    public struct TOOLINFO
+    {
+        public int cbSize;
+        public int uFlags;
+        public IntPtr hwnd;
+        public IntPtr uId;
+        public RECT rect;
+        public IntPtr hinst;
+
+        [MarshalAs(UnmanagedType.LPTStr)]
+        public string lpszText;
+
+        public IntPtr lParam;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct NMHDR
+    {
+        public IntPtr hwndFrom;
+        public IntPtr idFrom;
+        public int code;
+    }
+
+    [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+    public struct TOOLTIPTEXT
+    {
+        public NMHDR hdr;
+        public IntPtr lpszText;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
+        public string szText;
+        public IntPtr hinst;
+        public int uFlags;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WNDCLASSEX
+    {
+        public uint cbSize;
+        public ClassStyles style;
+        [MarshalAs(UnmanagedType.FunctionPtr)]
+        public Win32.WndProc lpfnWndProc;
+        public int cbClsExtra;
+        public int cbWndExtra;
+        public IntPtr hInstance;
+        public IntPtr hIcon;
+        public IntPtr hCursor;
+        public IntPtr hbrBackground;
+        public string lpszMenuName;
+        public string lpszClassName;
+        public IntPtr hIconSm;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WNDCLASS
+    {
+        public ClassStyles style;
+        [MarshalAs(UnmanagedType.FunctionPtr)]
+        public Win32.WndProc lpfnWndProc;
+        public int cbClsExtra;
+        public int cbWndExtra;
+        public IntPtr hInstance;
+        public IntPtr hIcon;
+        public IntPtr hCursor;
+        public IntPtr hbrBackground;
+        [MarshalAs(UnmanagedType.LPTStr)]
+        public string lpszMenuName;
+        [MarshalAs(UnmanagedType.LPTStr)]
+        public string lpszClassName;
+    }
+
+    /// <summary>
+    /// The mouse data structure
+    /// </summary>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct MouseInputData
+    {
+        /// <summary>
+        /// The x value, if ABSOLUTE is passed in the flag then this is an actual X and Y value
+        /// otherwise it is a delta from the last position
+        /// </summary>
+        public int dx;
+        /// <summary>
+        /// The y value, if ABSOLUTE is passed in the flag then this is an actual X and Y value
+        /// otherwise it is a delta from the last position
+        /// </summary>
+        public int dy;
+        /// <summary>
+        /// Wheel event data, X buttons
+        /// </summary>
+        public uint mouseData;
+        /// <summary>
+        /// ORable field with the various flags about buttons and nature of event
+        /// </summary>
+        public MouseEventFlags dwFlags;
+        /// <summary>
+        /// The timestamp for the event, if zero then the system will provide
+        /// </summary>
+        public uint time;
+        /// <summary>
+        /// Additional data obtained by calling app via GetMessageExtraInfo
+        /// </summary>
+        public IntPtr dwExtraInfo;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct KEYBDINPUT
+    {
+        public ushort wVk;
+        public ushort wScan;
+        public uint dwFlags;
+        public uint time;
+        public IntPtr dwExtraInfo;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct HARDWAREINPUT
+    {
+        public int uMsg;
+        public short wParamL;
+        public short wParamH;
+    }
+
+    /// <summary>
+    /// Captures the union of the three three structures.
+    /// </summary>
+    [StructLayout(LayoutKind.Explicit)]
+    public struct MouseKeybdhardwareInputUnion
+    {
+        /// <summary>
+        /// The Mouse Input Data
+        /// </summary>
+        [FieldOffset(0)]
+        public MouseInputData mi;
+
+        /// <summary>
+        /// The Keyboard input data
+        /// </summary>
+        [FieldOffset(0)]
+        public KEYBDINPUT ki;
+
+        /// <summary>
+        /// The hardware input data
+        /// </summary>
+        [FieldOffset(0)]
+        public HARDWAREINPUT hi;
+    }
+
+    /// <summary>
+    /// The Data passed to SendInput in an array.
+    /// </summary>
+    /// <remarks>Contains a union field type specifies what it contains </remarks>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct INPUT
+    {
+        /// <summary>
+        /// The actual data type contained in the union Field
+        /// </summary>
+        public SendInputEventType type;
+        public MouseKeybdhardwareInputUnion mkhi;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct TRACKMOUSEEVENT
+    {
+        public int cbSize;
+        public uint dwFlags;
+        public IntPtr hwndTrack;
+        public int dwHoverTime;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct DLLVersionInfo
+    {
+        public int cbSize;
+        public int dwMajorVersion;
+        public int dwMinorVersion;
+        public int dwBuildNumber;
+        public int dwPlatformID;
+    }
+}
